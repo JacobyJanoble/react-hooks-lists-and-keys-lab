c6da0f570ab8f1feb611a6fbb73fcf5377503fa2
Jacob <Jacob.McCann214@gmail.com>
2022-04-05 15:26:32 -0500

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // const linkMap = links.map((link) => {
+  //   return <a key={link} href={link}>{link}</a>
+  // });
+
+  return(
+    <nav>
+      <a href="#home" >{links[0]}</a>
+           <a href="#about" >{links[1]}</a>
+           <a href="#projects" >{links[2]}</a>
+    </nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
+function ProjectList({projects}) {
   console.log(projects);
+
+  const projectsMap = projects.map(project => {
+    return <span key={project}>{project}</span>
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <span>{projectsMap}</span>
+        </div>
     </div>
   );
 }
 
 export default ProjectList;
+
+
+// In the ProjectList component, a prop of projects is being passed down from the App component with an array of objects. For each object in the array, render one ProjectItem component with the correct props. Use the id of the project for the key prop.
